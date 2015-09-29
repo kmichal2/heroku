9b6560cbad585fd92d2e5311cc871563eddaf8c5
kmichal2 <mkovac@oralys.ca>
2015-09-29 11:19:31 -0400

    Update server.py

--- server.py
+++ server.py
@@ -75,7 +75,7 @@ def message():
   try:
     #resp.dial(to_val, callerId=caller_id)
     client.calls.create(from_=from_value,to=to_val,url="https://still-taiga-4190.herokuapp.com/call")
-    #resp.say("Thank you for contacting our sales department. Goodbye.", voice='alice')
+    resp.say("Thank you for contacting our sales department. Goodbye.", voice='alice')
   except Exception as e:
     app.logger.error(e)
     return jsonify({'error': str(e)})  
