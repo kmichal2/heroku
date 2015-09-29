ad73e627eafc6d89a1b30c1f9be547ddd30664cf
kmichal2 <mkovac@oralys.ca>
2015-09-29 11:02:58 -0400

    Update server.py

--- server.py
+++ server.py
@@ -83,7 +83,7 @@ def message():
   #message = client.messages.create(to=to_val, from_=from_value, body=body_txt)
   try:
     client.calls.create(from_=from_value,to=to_val,url="https://still-taiga-4190.herokuapp.com/message")
-    resp.say(body_txt, voice='alice')
+    resp.say("Thank you for contacting our sales department. Goodbye.", voice='alice')
   except Exception as e:
     app.logger.error(e)
     return jsonify({'error': str(e)})  
