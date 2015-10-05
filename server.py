9242b7cf7b1c033f78c1a8032cab364d61287214
kmichal2 <mkovac@oralys.ca>
2015-10-05 14:14:28 -0400

    Update server.py

--- server.py
+++ server.py
@@ -94,7 +94,7 @@ def message():
     try:
       #resp.dial(to_val, callerId=caller_id)
       #resp.say(message_txt, voice='alice')
-      client.calls.create(from_=from_value,to=to_val,url="https://still-taiga-4190.herokuapp.com/voice")
+      client.calls.create(from_=from_value,to=to_val,url="https://still-taiga-4190.herokuapp.com/message")
       #message = client.messages.create(to=to_val, from_=from_value, body=message_txt)
     except Exception as e:
       app.logger.error(e)
